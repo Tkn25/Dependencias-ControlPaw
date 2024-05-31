6f6dc37e2238ebe5e87fed640bb33cd611e1694d
Laura <104266213+Tkn25@users.noreply.github.com>
2024-05-31 17:26:38 +0200

    Update modificarMascota.php
    CAMBIOS PHP

--- PHP's/controlpaw/modificarMascota.php
+++ PHP's/controlpaw/modificarMascota.php
@@ -14,7 +14,9 @@ if ($conn->connect_error) {
 }
 
 // Recogemos los datos enviados mediante el método POST en variables
+$idMascota = $_POST['idMascota'];
 $nombre = $_POST['nombre'];
+$idDueno = $_POST['idDueno'];
 $idGenero = $_POST['idGenero'];
 $raza = $_POST['raza'];
 $idEspecie = $_POST['idEspecie'];
@@ -22,19 +24,22 @@ $peso = $_POST['peso'];
 $castrado = $_POST['castrado'];
 $fechaNacimiento = $_POST['fechaNacimiento'];
 $microchip = $_POST['microchip'];
+$enfermedad = $_POST['enfermedad'];
+$baja = $_POST['baja'];
 
-$idMascota = $_POST['idMascota'];
-
-// Construimos y ejecutamos la query de inserción de datos
+// Construimos y ejecutamos la query de actualización de datos
 $sql = "UPDATE mascotas 
-        SET nombre = '$nombre', 
-            idGenero = '$idGenero',
+        SET idDueño = $idDueno,
+            nombre = '$nombre',
+            idGenero = $idGenero,
             raza = '$raza',
-            idEspecie = '$idEspecie',
+            idEspecie = $idEspecie,
             peso = '$peso',
             castrado = '$castrado',
             fechaNacimiento = '$fechaNacimiento',
-            microchip = '$microchip'
+            microchip = '$microchip',
+            enfermedad = $enfermedad,
+            baja = $baja
         WHERE idMascota = $idMascota";
 
 if ($conn->query($sql) === TRUE) {
